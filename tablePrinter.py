1d432e2ab270688abf9736fb5e78f11e2aff0509
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-03-06 20:04:21 -0400

    Start project

---
+++ tablePrinter.py
@@ -0,0 +1,11 @@
+#! python3
+# tablePrinter.py - Display a list of lists of strings in
+# a well-organized table.
+
+tableData =  [['apples', 'oranges', 'cherries', 'banana'],
+                ['Alice', 'Bob', 'Carol', 'David'],
+                ['dogs', 'cats', 'moose', 'goose']]
+
+def printTable(array):
+    # To do
+    # Find the longest string
