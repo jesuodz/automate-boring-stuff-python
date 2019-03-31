2b8221f309ca1b1418bce9f9bd5a2885c4a2185e
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-03-31 10:15:31 -0400

    Add solution to project from chap. 6

--- tablePrinter.py
+++ tablePrinter.py
@@ -2,10 +2,34 @@
 # tablePrinter.py - Display a list of lists of strings in
 # a well-organized table.
 
-tableData =  [['apples', 'oranges', 'cherries', 'banana'],
+table_data =  [['apples', 'oranges', 'cherries', 'banana'],
                 ['Alice', 'Bob', 'Carol', 'David'],
                 ['dogs', 'cats', 'moose', 'goose']]
 
-def printTable(array):
-    # To do
-    # Find the longest string
+def print_table(main_arr):
+    col_widths = [0] * len(main_arr)
+
+    # Look for largest str length and save it in a column
+    for single_arr in main_arr:
+        curr_index = main_arr.index(single_arr)
+        for string in single_arr:
+            if len(string) > col_widths[curr_index]:
+                col_widths[curr_index] = len(string)
+
+    # Create a table from array `(x, y)` => `(y, x)`
+    table = []
+    for x in range(len(main_arr[0])):
+        temp_arr = []
+        for y in range(len(main_arr)):
+            temp_arr.append(main_arr[y][x])
+        table.append(temp_arr)
+        temp_arr = []
+
+    # Print pretty table
+    for arr in table:
+        for string in arr:
+            str_index = arr.index(string)
+            print(string.rjust(col_widths[str_index] + 1), end='')
+        print()
+
+print_table(table_data)
