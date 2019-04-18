dfff447aaa9817316eb22d8dfc3f9ac52229ab69
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-04-18 11:23:15 -0400

    Create research.py

---
+++ automate-boring-stuff-python/research.py
@@ -0,0 +1,17 @@
+#! python3
+# research.py - Look for regex patterns in .txt files
+
+import re, os
+
+files = [file for file in os.listdir() if file.endswith('txt')]
+
+pattern = input('Regex pattern: ')
+regex = re.compile(r'%s' % pattern)
+
+# TODO: Loop through all files in a efficient manner
+
+# TODO: Find and save matches
+
+""" IDEA: Save matches by file and line in a dict
+         { 'FILE' : ['LINE1', 'LINE2', 'LINE4'], ...}
+"""
