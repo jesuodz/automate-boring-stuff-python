7467f06d64f1f4e24f982c94f38e827193f19ccf
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-04-21 22:56:54 -0400

    Loop over content of every .txt file

--- automate-boring-stuff-python/research.py
+++ automate-boring-stuff-python/research.py
@@ -1,7 +1,8 @@
 #! python3
 # research.py - Look for regex patterns in .txt files
 
-import re, os
+import re
+import os
 
 files = [file for file in os.listdir() if file.endswith('txt')]
 
@@ -9,9 +10,16 @@ pattern = input('Regex pattern: ')
 regex = re.compile(r'%s' % pattern)
 
 # TODO: Loop through all files in a efficient mannerk
+for file in files:
+    fhandler = open(file, 'r', encoding='utf-8')
+    content = fhandler.read()
+
+    print(regex.findall(content))
+    fhandler.close()
 
 # TODO: Find and save matches
 
 """ IDEA: Save matches by file and line in a dict
          { 'FILE' : ['LINE1', 'LINE2', 'LINE4'], ...}
 """
+input()
