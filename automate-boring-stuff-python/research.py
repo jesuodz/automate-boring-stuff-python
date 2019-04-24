19f1cc464449e7e321228bb2aa68b4b024757225
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-04-23 20:23:01 -0400

    Raise default exit exceptions

--- automate-boring-stuff-python/research.py
+++ automate-boring-stuff-python/research.py
@@ -3,10 +3,15 @@
 
 import re
 import os
+from sys import exit
 
 files = [file for file in os.listdir() if file.endswith('txt')]
 
+if len(files) == 0: exit('No "*.txt" files found in folder.')
+
 pattern = input('Regex pattern: ')
+if len(pattern) == 0: exit('Pattern not long enough.')
+
 regex = re.compile(r'%s' % pattern)
 
 # TODO: Loop through all files in a efficient mannerk
@@ -22,4 +27,3 @@ for file in files:
 """ IDEA: Save matches by file and line in a dict
          { 'FILE' : ['LINE1', 'LINE2', 'LINE4'], ...}
 """
-input()
