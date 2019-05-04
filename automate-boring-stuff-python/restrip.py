d11d5fb489f66d9890ce15f808afbb7fc5021d43
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-05-04 11:36:35 -0400

    Add exception when no arguments are passed

--- automate-boring-stuff-python/restrip.py
+++ automate-boring-stuff-python/restrip.py
@@ -3,6 +3,11 @@
 # Usage:       python restrip.py "   textt ..  " - Strip trailing whitespaces.
 #              python restrip.py "hello world" "hello "  - Strip later text.
 
+"""
+BUG:
+    -   python restrip.py '  '
+"""
+
 import re
 import sys
 
@@ -10,14 +15,18 @@ word_re = re.compile(r'\S+((\s+\S+)?)+')
 
 def strip(s, c):
     # Delete whitespace characters from beginning and end
-    if not c: return word_re.search(s).group()
+    if not c: return repr(word_re.search(s).group())
 
     # Delete character from beginning and end
     char_re = re.compile(r'(^%s)|(%s$)' % (c, c))
-    return char_re.sub('', s)
+    return repr(char_re.sub('', s))
 
 if len(sys.argv) >= 1:
-    string = sys.argv[1]
+    try:
+        string = sys.argv[1]
+    except IndexError:
+        sys.exit("Error: 'restrip.py' receives at least 1 argument.")
+
     char = None if len(sys.argv) == 2 else sys.argv[2]
 
-    print(repr(strip(sys.argv[1], char)))
+    print(strip(sys.argv[1], char))
