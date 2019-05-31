a4926252516767a969e176508b20eb39ade9b0d5
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-05-30 22:34:43 -0400

    Refractor

--- neural-networks/and-gate.py
+++ neural-networks/and-gate.py
@@ -1,10 +1,8 @@
 import numpy as np
 
-def heavyside(c):
-    # funcion signo
-    if c < 0:
-        return 0
-    return 1
+def heavyside(n):
+    """ funcion signo """
+    return 1 if n > 0 else 0
 
 class Neuron:
     def __init__(self, x, umbral):
@@ -13,11 +11,15 @@ class Neuron:
         self.weights    = np.full(self.input.shape[1], 0.5)
         self.output     = np.zeros(self.input.shape[0])
 
-    def calc(self):
-        cross_mult = np.dot(self.input, self.weights) - self.u
-        self.output = list(map(heavyside, cross_mult))
+    def feedforward(self):
+        """ Calcula la salida """
 
+        # Determina el coste
+        cost = np.dot(self.input, self.weights) - self.u
+        self.output = list(map(heavyside, cost))
+
+# Esperado: [1, 0, 0, 0]
 X = np.array([[1,1], [0,1], [0,0], [1,0]])
 n = Neuron(X, 0.75)
-n.calc()
+n.feedforward()
 print(n.output)
