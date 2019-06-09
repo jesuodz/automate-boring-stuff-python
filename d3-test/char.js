803d7270505535855bf73974ffffff211e49d3b4
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-06-09 16:34:07 -0400

    Fix char.js

--- d3-test/char.js
+++ d3-test/char.js
@@ -2,7 +2,7 @@ const margin = {top:10, bottom: 30, right: 40, left: 30},
       width = 450 - margin.left - margin.right,
       height = 400 - margin.bottom - margin.top;
 
-let svg = d3.select("#visualization_area")
+let svg = d3.select("#vis_area")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.bottom + margin.top)
@@ -11,7 +11,7 @@ let svg = d3.select("#visualization_area")
 
 // Crea la escala: transforma valor a pixel
 let x = d3.scaleLinear()
-  .domain([-8, 8])
+  .domain([-7, 7])
   .range([0, width]);
 svg
   .append('g')
@@ -19,8 +19,17 @@ svg
   .call(d3.axisBottom(x));
 
 let y = d3.scaleLinear()
-  .domain([-8, 8])
+  .domain([-7, 7])
   .range([height, 0]);
 svg
   .append('g')
-  .call(d3.axisLeft(y))
+  .call(d3.axisRight(y))
+
+svg
+  .selectAll("whatever")
+  .data(data)
+  .enter()
+  .append("circle")
+    .attr("cx", function(d){ return x(d.x) })
+    .attr("cy", function(d){ return y(d.y) })
+    .attr("r", 1)
